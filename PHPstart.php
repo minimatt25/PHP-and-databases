631cb839d57aa1844287eb81279ac93ad9fd07ce
Matt Adam <95365935+minimatt25@users.noreply.github.com>
2024-10-06 15:24:54 +0100

    PHP start and SQL databases

---
+++ PHPstart.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <style>
+        body{
+            background-color: #E7ECEF;
+            font-family: "Quicksand", sans-serif;
+        }
+
+        .date{
+            color: #272932;
+            font-size: 40px;
+            padding: 0;
+            margin: 0;
+        }
+    </style>
+
+    <?php 
+    //date
+    date_default_timezone_set("Europe/London");
+    echo '<div class="date">'.date("d-m-y")."</div>";
+
+    //read file, and write back +=1
+    $fileContent = file("test.txt");
+    $times = (int)$fileContent[1];
+    $times+=1;
+    $fileContent[1] = $times; 
+    file_put_contents("test.txt", implode("",$fileContent));
+    echo("<br>since making this line i have visited this page " . $times . " times!");
+    
+    /*
+    //basic append to screen
+    echo "<br>hello!<br>";
+
+    //variable declaration and use
+    $food =  "cheese";
+    echo "i love " . $food . "<br>";
+
+    //open file, read, and append to screen
+    $testFile = fopen("test.txt", "r");
+    echo fread($testFile, filesize("test.txt")-3);
+    fclose($testFile);
+    */
+    ?>
+    <br><br><br>
+    <?php
+        $connection = new mysqli("localhost","root","","test");
+        $result = $connection->query("SELECT * FROM people");
+        echo "<table border='1'>";
+        echo "<tr><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+
+        while($row = $result->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row['firstName'] . "</td>";
+            echo "<td>" . $row['lastName'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        $connection->close();
+        echo "<br> table inserted from connected database"
+    ?>
+
+</body>
+</html>
---
+++ Second try.session.sql
@@ -0,0 +1,4 @@
+INSERT INTO people (firstName, lastName, email)
+VALUES 
+    ('John','Smith','JSmith@gmail.com'),
+    ('Matthew', 'Adam', 'matt123@gmail.com');
